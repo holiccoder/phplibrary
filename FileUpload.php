236c1428b53be872c3ff4d44b5a63833435b8370
sachu <sacrg793@aliyun.com>
2019-05-12 16:40:41 +0800

    final one

--- FileUpload.php
+++ FileUpload.php
@@ -3,52 +3,36 @@
 class FileUpload {
 
     public $files;
-    const maxFileSize = '29999';
+    const maxFileSize = '38292722';
     const uploadDir = 'uploads/';
+    const imageFileExtensions = "jpg,jpeg,png,svg,gif";
     const allowedExtensions = "jpg,png,gif";
-    const allowedExtesionsError = "这种类型的文件不可上传！";
-    const allowedFileSizeError = "文件大小超过允许范围";
+    const allowedExtesionsError = "this type of file could not be uploaded";
+    const allowedFileSizeError = "Your file has exeeded the limit of the filesize";
+    const fileNameSize = 6;
 
-    public function __construct($_FILES){
-        $this->file = $_FILES;
+    public function __construct($FILES){
+        $this->files = $FILES;
     }
 
     //检查上传的文件类型是否在允许的文件类型范围内
     function checkAllowedFileExtensions(){
-        if($this->files){
-            $files = $this->files;
+        $files = $this->files;
+        if($files){
             $allowedExts = $this->commaDelimitedStringsToArray($this::allowedExtensions);
+            $checkSuccess = false;
             foreach($files as $key => $value){
                  $filename = $_FILES[$key]["name"];
                  $extension = $this->getUploadedFileExtension($filename);
                  if(in_array($extension, $allowedExts) == false){
                      return $this::allowedExtesionsError;
                  }else{
-                     return true;
+                     $checkSuccess = true;
                  }
             }
-        }
-    }
 
-    function fileUpload(){
-        if($this->checkAllowedFileExtensions() && $this->checkFileSize()){
-             $files = $this->files;
-               foreach($files as $key => $value){
-                    $tmpname = $files[$key]["tmp_name"];
-                    $filename = $files[$key]["name"];
-                    $newfilename = $this->renameFile($filename);
-                    $destinationfile = $this::uploadDir.$newfilename;
-                    move_uploaded_file($tmpname, $destinationfile);
-                    echo "上传成功！";
-               }
-        }else{
-            echo $this->checkAllowedFileExtensions();
-            echo $this->checkFileSize();
+            return $checkSuccess;
         }
-    }
-
-    function getUploadedFileExtension($filename){
-        return substr($filename, strrpos($filename, '.')+1);
     }
 
     function checkFileSize(){
@@ -67,9 +51,32 @@ class FileUpload {
 
     function renameFile($originalFilename){
         $extension = $this->getUploadedFileExtension($originalFilename);
-        $newFileName = $this->generateRandomString(6);
+        $newFileName = $this->generateRandomString($this::fileNameSize);
         return $newFileName.".".$extension;
     }
+
+    function fileUpload(){
+        if($this->checkAllowedFileExtensions() == 1 && $this->checkFileSize() == 1){
+               $files = $this->files;
+
+               foreach($files as $key => $value){
+                    $tmpname = $files[$key]["tmp_name"];
+                    $filename = $files[$key]["name"];
+                    $newfilename = $this->renameFile($filename);
+                    $destinationfile = $this::uploadDir.$newfilename;
+                    move_uploaded_file($tmpname, $destinationfile);
+               }
+               return "File Uploaded";
+        }else{
+            echo $this->checkAllowedFileExtensions();
+            echo $this->checkFileSize();
+        }
+    }
+
+    function getUploadedFileExtension($filename){
+        return substr($filename, strrpos($filename, '.')+1);
+    }
+
 //$strings可以是一个也可以是多个，都会转化为数组，如果多个的话用英文逗号隔开，它会遍历返回
     function commaDelimitedStringsToArray($commaDelimitedStrings){
         if(strpos($commaDelimitedStrings, ",") === false){
@@ -90,4 +97,3 @@ class FileUpload {
         return $randomString;
     }
 }
-
--- test.php
+++ test.php
@@ -2,4 +2,7 @@
 
 //$strings可以是一个也可以是多个，如果多个的话用英文逗号隔开，它会遍历
 
-print_r($_FILES);
+require_once "FileUpload.php";
+
+$fileupload = new FileUpload($_FILES);
+echo $fileupload->checkAllowedFileExtensions();
