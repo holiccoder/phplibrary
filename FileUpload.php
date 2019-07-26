7921b93fa83279f4f4490619086e0a2fe540faf7
sachu <38061345+holiccoder@users.noreply.github.com>
2019-07-26 11:41:15 +0800

    Update FileUpload.php

--- FileUpload.php
+++ FileUpload.php
@@ -106,4 +106,43 @@ class FileUpload {
         }
         return $randomString;
     }
+    function curlFileUpload(){
+        $dataFile      = 'upload.txt';
+$sftpServer    = '216.107.128.134';
+$sftpUsername  = 'root';
+$sftpPassword  = 'ranger1984@#$';
+$sftpPort      = 22;
+$sftpRemoteDir = '/root';
+ 
+$ch = curl_init('sftp://' . $sftpServer . ':' . $sftpPort . $sftpRemoteDir . '/' . basename($dataFile));
+ 
+$fh = fopen($dataFile, 'r');
+ 
+if ($fh) {
+    curl_setopt($ch, CURLOPT_USERPWD, $sftpUsername . ':' . $sftpPassword);
+    curl_setopt($ch, CURLOPT_UPLOAD, true);
+    curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_SFTP);
+    curl_setopt($ch, CURLOPT_INFILE, $fh);
+    curl_setopt($ch, CURLOPT_INFILESIZE, filesize($dataFile));
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+ 
+    $verbose = fopen('php://temp', 'w+');
+    curl_setopt($ch, CURLOPT_STDERR, $verbose);
+ 
+    $response = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+ 
+    if ($response) {
+        echo "Success";
+    } else {
+        echo "Failure";
+        rewind($verbose);
+        $verboseLog = stream_get_contents($verbose);
+        echo "Verbose information:\n" . $verboseLog . "\n";
+    }
+  }
+    }
+    
+
 }
